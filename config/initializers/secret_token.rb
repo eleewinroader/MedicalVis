a5995443615c3d472e1dc2ed3102c1c75bb7e107
ShinpeiKawahito <kawahito.shinpei@gmail.com>
2014-02-24 12:43:08 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedicalVis::Application.config.secret_token = 'e070962e898086f1a2b25c5a2e7f627256d05429bba3a48ca1347d0b2b25a52bdad62b6d56ac540d2816d9ad51e22f3e6c089400e3ba5a7755851d070ae56ea2'
